6ca3ae6b397bdb8b7bc89e7d3732f864e875dc4a
Avyaan <Avyaan@gmail.com>
2024-08-02 11:06:50 +0530

    biodata

---
+++ neelam biodata/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neelam_biodata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my name is neelam");
+            Console.WriteLine("my age is =35");
+            Console.WriteLine("phone no is 43");
+            Console.ReadLine();
+        }
+    }
+}
